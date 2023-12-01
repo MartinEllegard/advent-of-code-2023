f730a71c52cfa34408baf7ac38e7ebfdb5173e37
Martin Ellegård <martin.ellegard@gmail.com>
2023-12-01 17:57:21 +0100

    Day 1 solved

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,43 +1,8 @@
 #![feature(test)]
 
-use lazy_static::lazy_static;
-use regex::Regex;
 use std::u32;
-use strum::{IntoEnumIterator, VariantNames};
-use strum_macros::{EnumString, EnumVariantNames};
+
 extern crate test;
-
-#[derive(EnumString, Debug, EnumVariantNames)]
-enum Digits {
-    #[strum(serialize = "one", serialize = "1")]
-    One,
-    #[strum(serialize = "two", serialize = "2")]
-    Two,
-    #[strum(serialize = "three", serialize = "3")]
-    Three,
-    #[strum(serialize = "four", serialize = "4")]
-    Four,
-    #[strum(serialize = "five", serialize = "5")]
-    Five,
-    #[strum(serialize = "six", serialize = "6")]
-    Six,
-    #[strum(serialize = "seven", serialize = "7")]
-    Seven,
-    #[strum(serialize = "eight", serialize = "8")]
-    Eight,
-    #[strum(serialize = "nine", serialize = "9")]
-    Nine,
-}
-
-lazy_static! {
-    static ref FIRST_DIGIT: Regex = {
-        let digits = [
-            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "[1-9]",
-        ];
-        let expression = digits.join("|");
-        Regex::new(expression.as_str()).unwrap()
-    };
-}
 
 #[bench]
 fn name(b: &mut test::Bencher) {
@@ -52,22 +17,16 @@ fn main() {
 }
 
 fn part1(input: &str) -> u32 {
-    let lines = input.lines();
-    let mut output = Vec::<u32>::with_capacity(lines.clone().count());
-    for line in lines {
-        let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
-        match digits.len() {
-            0 => {
-                continue;
-            }
-            1 => output.push(*digits.first().unwrap()),
-            _ => {
-                let out = [digits.first().unwrap(), digits.last().unwrap()]
-                    .iter()
-                    .fold("".to_string(), |acc, x| acc + &x.to_string());
-                output.push(out.parse::<u32>().unwrap());
-            }
-        }
-    }
-    output.iter().sum()
+    input
+        .lines()
+        .map(|line| {
+            let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
+            let first = digits.first().expect("must contain a digit");
+            let out = match digits.last() {
+                Some(last) => format!("{0}{1}", first, last),
+                None => format!("{0}{1}", first, first),
+            };
+            out.parse::<u32>().expect("Must be a number")
+        })
+        .sum()
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,17 +1,57 @@
 #![feature(test)]
+
 extern crate test;
 
 #[bench]
 fn name(b: &mut test::Bencher) {
     let input = include_str!("input1.txt");
-    b.iter(|| part1(input))
+    b.iter(|| part2(input))
 }
 
 fn main() {
     let input = include_str!("input1.txt");
-    let _result = part1(input);
+    let result = part2(input);
+    println!("result is: {:?}", result);
 }
 
-fn part1(input: &str) -> String {
-    input.to_string()
+fn part2(input: &str) -> u32 {
+    input.lines().map(|line| extract(line)).sum()
+}
+
+fn extract(line: &str) -> u32 {
+    let mut fixed_line = (0..line.len()).filter_map(|index| {
+        let line_from_index = &line[index..];
+        let result = if line_from_index.starts_with("one") {
+            '1'
+        } else if line_from_index.starts_with("two") {
+            '2'
+        } else if line_from_index.starts_with("three") {
+            '3'
+        } else if line_from_index.starts_with("four") {
+            '4'
+        } else if line_from_index.starts_with("five") {
+            '5'
+        } else if line_from_index.starts_with("six") {
+            '6'
+        } else if line_from_index.starts_with("seven") {
+            '7'
+        } else if line_from_index.starts_with("eight") {
+            '8'
+        } else if line_from_index.starts_with("nine") {
+            '9'
+        } else {
+            line_from_index.chars().next().unwrap()
+        };
+
+        result.to_digit(10)
+    });
+
+    let first = fixed_line.next().expect("Must be a number");
+
+    match fixed_line.last() {
+        Some(last) => format!("{0}{1}", first, last),
+        None => format!("{0}{1}", first, first),
+    }
+    .parse::<u32>()
+    .unwrap()
 }
